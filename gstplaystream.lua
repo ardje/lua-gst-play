a8e8c96ade394322537a51484edb11cc018d62de
Root DC0 kwaak <root@u3>
2016-01-29 03:21:36 +0100

    How to set flags?

--- gstplaystream.lua
+++ gstplaystream.lua
@@ -8,6 +8,8 @@
 local lgi = require 'lgi'
 local GLib = lgi.GLib
 local Gst = lgi.Gst
+local Enum=require'lgi.enum'
+
 
 local main_loop = GLib.MainLoop()
 
@@ -32,9 +34,17 @@ local function bus_callback(bus, message)
 end
 
 local play = Gst.ElementFactory.make('playbin', 'play')
+local sink = Gst.ElementFactory.make('cluttersink', 'sink')
 play.uri = 'file:///home/dr/data/ICE2015_led.mov'
-play.flags=0x253
-play["video-sink"]="cluttersink"
+play.video_sink=sink
+print('play:',play:_get'flags')
+--play.flags:set(0x253)
+--print(play.flags)
+--play.flags=595
+--play.flags=0x253 == GST_PLAY_FLAG_DEINTERLACE|GST_PLAY_FLAG_NATIVE_VIDEO|GST_PLAY_FLAG_SOFT_VOLUME|GST_PLAY_FLAG_AUDIO|GST_PLAY_FLAG_VIDEO
+
+--play.flags={'GST_PLAY_FLAG_DEINTERLACE','GST_PLAY_FLAG_NATIVE_VIDEO','GST_PLAY_FLAG_SOFT_VOLUME','GST_PLAY_FLAG_AUDIO','GST_PLAY_FLAG_VIDEO'}
+--play.flags='GST_PLAY_FLAG
 --play.uri = 'http://www.cybertechmedia.com/samples/raycharles.mov'
 play.bus:add_watch(GLib.PRIORITY_DEFAULT, bus_callback)
 play.state = 'PLAYING'
